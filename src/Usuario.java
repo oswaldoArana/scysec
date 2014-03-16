40047113feb7d1a3f1db94acee4ac677b0902d17
xps <xps@debian>
2014-03-16 05:36:51 -0600

    ARCHIVO JAR DE ENTIDADES

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,9 @@
  * @author xps
  */
 public class Usuario {
+    
+   public static void main(String[] args){
+       JOptionPane.showMessageDialog(null,";)");
+   }
     
 }
